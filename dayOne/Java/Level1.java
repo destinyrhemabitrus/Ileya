7f1cdabfcf8f9df7539ca4c859158ff5c6389425
destinyrhemabitrus <destinyrhemabitrus@gmail.com>
2026-05-26 16:57:10 +0100

    Recent update

---
+++ dayOne/Java/Level1.java
@@ -0,0 +1,115 @@
+import java.util.Arrays;
+public class Level1{
+
+
+    public static void main(String[] args){
+        int[] arr = {45, 3, 0, 45, 3,9};
+        
+        System.out.println(Arrays.toString(arrayInverter(arr)));
+
+    }
+
+//    Helper method
+    public static int evenArrCount(int[] arr){
+
+        int count = 0;
+        for(int counter = 0; counter < arr.length; counter++){
+
+            if(arr[counter]%2 == 0){
+            
+                count = count + 1;    
+        
+            }
+
+        }
+
+        return count;
+
+    }
+
+
+//    Helper method
+    public static int oddArrCount(int[] arr){
+
+        int count = 0;
+        for(int counter = 0; counter < arr.length; counter++){
+
+            if(arr[counter]%2 != 0){
+            
+                count = count + 1;    
+        
+            }
+
+        }
+
+        return count;
+
+    }
+
+
+//
+    public static int[][] arraySplitter(int[] arr){
+
+        int evenCount =  evenArrCount(arr);
+        int oddCount =  oddArrCount(arr);
+        
+        int rowOne = 0;
+        int rowTwo = 1;
+        int columnOne = 0;
+        int columnTwo = 0;
+        int[][] splitted = new int[2][];
+        splitted[0] = new int[oddCount];
+        splitted[1] = new int[evenCount];
+
+        for(int counter = 0; counter < arr.length; counter++){
+
+            if(arr[counter]%2 != 0){
+            
+                splitted[rowOne][columnOne] = arr[counter];
+                columnOne = columnOne + 1;   
+        
+            }else{
+
+                splitted[rowTwo][columnTwo] = arr[counter];
+                columnTwo = columnTwo + 1;       
+            
+            }
+
+        }
+ 
+        return splitted;
+
+    }
+
+
+
+
+//---------------Question 2------------------Parlindrome
+
+//    Helper method
+    public static int[] arrayInverter(int[] arr){
+
+        int[] output = new int[arr.length];
+        int index = 0;
+        for(int counter = arr.length - 1; counter >= 0; counter--){
+
+            output[index] = arr[counter]; 
+            index = index + 1;
+        }
+
+        return output;
+
+    }
+
+
+//------------parlindromeChecker
+
+
+    public static boolean parlindromeChecker(int[] arr){
+
+        int[] invertedArray = arrayInverter(arr);
+        return Arrays.equals(invertedArray, arr);
+    }
+
+
+}
---
+++ dayOne/Java/Level1Test.java
@@ -0,0 +1,121 @@
+public class Level1Test{
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_evenArrCountMethod_Exists(){
+
+        int[] arr = {45,60,3,10,9,22};
+        Level1.evenArrCount(arr);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_evenArrCountMethod_ReturnsEvenCount(){
+
+        int[] arr = {45,60,3,10,9,22,40,6,8};
+        int actual = Level1.evenArrCount(arr);
+        int expected = 6;
+        org.junit.jupiter.api.Assertions.assertEquals(expected,actual);
+
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_oddArrCountMethod_Exists(){
+
+        int[] arr = {45,60,3,10,9,22};
+        Level1.oddArrCount(arr);
+
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_oddArrCountMethod_ReturnsOddCount(){
+
+        int[] arr = {45,60,3,10,9,22,40,6,8,3};
+        int actual = Level1.oddArrCount(arr);
+        int expected = 4;
+        org.junit.jupiter.api.Assertions.assertEquals(expected,actual);
+
+    }
+
+
+//-----------arraySplitter--------------
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_arraySplitterMethod_Exists(){
+
+        int[] arr = {45,60,3,10,9,22};
+        Level1.arraySplitter(arr);
+
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_arraySplitterMethod_ReturnsSplitted2D_OddAndEvenArray(){
+
+        int[] arr = {45,60,3,10,9,22,16,11,13,15};
+        int[][] actual = Level1.arraySplitter(arr);
+        int[][] expected = {{45,3,9,11,13,15},{60,10,22,16}};
+        org.junit.jupiter.api.Assertions.assertArrayEquals(expected,actual);
+
+    }
+
+
+
+//-----------Question 2 ---------helper method
+
+    @org.junit.jupiter.api.Test
+    public void Test_That_arrayInverterMethod_Exists(){
+
+        int[] arr = {45, 0, 8, 0, 45};
+        Level1.arrayInverter(arr);
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_arrayInverterMethod_ReturnsInvertedArrayElements(){
+
+        int[] arr = {45, 0, 8, 0, 4, 6};
+        int[] actual = Level1.arrayInverter(arr);
+        int[] expected = {6, 4, 0, 8, 0, 45};
+        org.junit.jupiter.api.Assertions.assertArrayEquals(expected,actual);
+
+    }
+
+//------------parlindrome checker---------
+    @org.junit.jupiter.api.Test
+    public void Test_That_parlindromeCheckerMethod_Exists(){
+
+        int[] arr = {45, 0, 8, 0, 45};
+        Level1.parlindromeChecker(arr);
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_parlindromeCheckerMethod_withParlindromeArray_ReturnsTrue(){
+
+        int[] arr = {45, 0, 8, 0, 45};
+        boolean actual = Level1.parlindromeChecker(arr);
+        org.junit.jupiter.api.Assertions.assertTrue(actual);
+
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void Test_parlindromeCheckerMethod_withNonParlindromeArray_ReturnsFalse(){
+
+        int[] arr = {45, 0, 8, 0, 4};
+        boolean actual = Level1.parlindromeChecker(arr);
+        org.junit.jupiter.api.Assertions.assertFalse(actual);
+
+
+    }
+
+
+}
